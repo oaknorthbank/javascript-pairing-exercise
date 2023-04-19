815fa75278bd84d8f8bd093705242b104e91c621
Meera Patel <meera@Meeras-MacBook-Pro.local>
2023-04-19 14:19:43 +0100

    Get the invoices as a list of objects:

--- index.test.js
+++ index.test.js
@@ -1,14 +1,30 @@
-import fs from 'fs/promises';
-import { parse } from 'csv';
+import fs from 'fs';
+import { parse } from 'csv-parse';
 import { consume_invoices } from './consumer.js';
 
-describe('consume_invoices', () => {
-  let invoices;
-  beforeEach(async () => {
-    const csv = await fs.readFile('invoices.csv', 'utf8');
-    invoices = await parse(csv);
+async function loadCsvFile(filePath) {
+  const results = [];
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(filePath)
+      .pipe(parse({ delimiter: ',', columns: true }))
+      .on('data', (data) => results.push(data))
+      .on('end', () => resolve(results))
+      .on('error', reject);
   });
-  test('empty list', () => {
+}
+
+
+describe('Consume invoices', () => {
+  let invoices;
+  
+  beforeEach(async () => {
+    invoices = await loadCsvFile('invoices.csv');
+
+  });
+  
+  it('should do nothing when there are no invoices', () => {
     consume_invoices([]);
   });
+
 });
+
